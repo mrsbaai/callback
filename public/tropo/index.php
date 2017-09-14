e54df64a9660190af989f2afabfc2ff016cae964
mrsbaai <abdelilah.sbaai@gmail.com>
2017-09-14 14:06:05 +0100

    log errors

--- public/tropo/index.php
+++ public/tropo/index.php
@@ -1,1 +1,34 @@
-<?php phpinfo(); ?>
+<?php
+
+ini_set("log_errors", 1);
+ini_set("error_log", "php-error.log");
+error_log( "Hello, errors!" );
+
+include('../include.php');
+
+require('tropo.class.php');
+
+        $from = null;
+        $to = null;
+        $text = null;
+
+        $session = new Session();
+
+        $text = $session->getInitialText();
+        $from = $session->getFrom();
+        $to = $session->getTo();
+		sendCallback($from,$to,$text);
+		
+        if ($from <> null and $to <> null and $text <> null){
+            $text = urlencode($text);
+			
+			
+			return "cool!";
+        }else{
+            return "not cool";
+        }
+
+
+
+
+?>
