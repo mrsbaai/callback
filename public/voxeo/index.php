5d465341b1cb5ed9c3610646b38e86b202387c57
mrsbaai <abdelilah.sbaai@gmail.com>
2017-09-16 16:24:22 +0100

    voxeo callback

---
+++ public/voxeo/index.php
@@ -0,0 +1,27 @@
+<?php
+
+
+// log voxeo messages
+
+ini_set("log_errors", 1);
+ini_set("error_log", "php-error.log");
+
+include('../include.php');
+
+        $from = null;
+        $to = null;
+        $text = null;
+		
+		if(isset($_POST["user"])) $from=$_POST["user"];
+		if(isset($_POST["to"])) $to=$_POST["to"];
+		if(isset($_POST["msg"])) $text=$_POST["msg"];
+		
+		if ($from <> null and $to <> null and $text <> null){
+            $text = urlencode($text);
+			if (is_array($from)){$from = reset($from);}
+			if (is_array($to)){$to = reset($to);}
+			sendCallback($from,$to,$text);
+			return "OK";
+        }else{
+			error_log("Something messing");
+        }
