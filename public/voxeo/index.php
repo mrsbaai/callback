5a9406fb5a78f8a1b71317ae908d7373e7fceee0
mrsbaai <abdelilah.sbaai@gmail.com>
2017-09-21 17:25:15 +0100

    ...

--- public/voxeo/index.php
+++ public/voxeo/index.php
@@ -19,8 +19,7 @@ include('../include.php');
 		if(isset($_POST["to"])) $to=$_POST["to"];
 		if(isset($_POST["msg"])) $text=$_POST["msg"];
 		
-		// show msg in error log to test
-		error_log($text);
+
 		
 		if ($from <> null and $to <> null and $text <> null){
             $text = urlencode($text);
